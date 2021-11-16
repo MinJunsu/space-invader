aba8376886e2e1068cf1890f0471d671b5435a58
MinJunsu <sunsuking@gmail.com>
2021-11-17 01:00:06 +0900

    [CU-1quwwrz] Fix left & right assets in birds

--- models/bullets.py
+++ models/bullets.py
@@ -1,4 +1,5 @@
 from os.path import join
+from random import randint
 
 from pygame.mixer import Sound
 
@@ -11,15 +12,32 @@ class Bullet(Entity):
         self.pos_x = pos_x
         self.pos_y = pos_y
 
-    def move(self) -> None:
-        self.rect.y -= self.speed
-
     def set_sound(self, name):
         super(Bullet, self).set_sound(name)
         self.sound.play()
 
 
-class PlaneBullet(Bullet):
+class PlayerBullet(Bullet):
+    def __init__(self, pos_x, pos_y):
+        super(PlayerBullet, self).__init__(pos_x, pos_y)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def move(self) -> None:
+        self.rect.y -= self.speed
+
+
+class EnemyBullet(Bullet):
+    def __init__(self, pos_x, pos_y):
+        super(EnemyBullet, self).__init__(pos_x, pos_y)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def move(self) -> None:
+        self.rect.y += randint(1, self.speed)
+
+
+class PlaneBullet(PlayerBullet):
     def __init__(self, pos_x, pos_y):
         super(PlaneBullet, self).__init__(pos_x, pos_y)
         self.set_images('plane_bullet')
@@ -28,12 +46,25 @@ class PlaneBullet(Bullet):
         self.set_sound('plane_bullet.wav')
 
 
-class SpaceShipBullet(Bullet):
+class BlueSpaceShipBullet(EnemyBullet):
     def __init__(self, pos_x, pos_y):
-        super(SpaceShipBullet, self).__init__(pos_x, pos_y)
-        self.set_images('plane_bullet')
-        self.set_sound('spaceship_bullet.wav')
+        super(BlueSpaceShipBullet, self).__init__(pos_x, pos_y)
+        self.speed = 3
+        self.set_images('blue_spaceship_bullet')
+        self.set_sound('blue_spaceship.wav')
 
 
-class PassBullet(Bullet):
-    pass
+class RedSpaceShipBullet(EnemyBullet):
+    def __init__(self, pos_x, pos_y):
+        super(RedSpaceShipBullet, self).__init__(pos_x, pos_y)
+        self.speed = 5
+        self.set_images('red_spaceship_bullet')
+        self.set_sound('red_spaceship.wav')
+
+
+class GreenSpaceShipBullet(EnemyBullet):
+    def __init__(self, pos_x, pos_y):
+        super(GreenSpaceShipBullet, self).__init__(pos_x, pos_y)
+        self.speed = 7
+        self.set_images('green_spaceship_bullet')
+        self.set_sound('green_spaceship.wav')
--- models/space_ship.py
+++ models/space_ship.py
@@ -1,26 +1,63 @@
-from pygame.sprite import Sprite
-from .entity import Entity
+from random import randint
+from pygame.sprite import Group
+
+from .base import Enemy
+from .explosions import BirdExplosion
+from .bullets import BlueSpaceShipBullet, RedSpaceShipBullet, GreenSpaceShipBullet
 
 
-class SpaceShip(Entity):
-    DEFAULT_COUNT = 0
-    IMAGE_PATH = 'spaceships'
+class SpaceShip(Enemy):
+    COOLDOWN = 100
 
-    def __init__(self):
-        super(SpaceShip, self).__init__()
+    def __init__(self, pos_x, pos_y):
+        super(SpaceShip, self).__init__('spaceships')
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.health_point = 1
+        self.explosion = BirdExplosion
+        self.weapon = None
+        self.weapons = Group()
+        self.cool_down_counter = 0
 
-    def is_collision(self, sprites: Sprite):
-        if super().is_collision(sprites):
-            pass
+    def update(self, *args, **kwargs) -> None:
+        self.cool_down_counter += randint(0, 1)
+        self.attack()
+        super(SpaceShip, self).update()
+
+    def attack(self) -> None:
+        if self.cool_down_counter > self.COOLDOWN:
+            self.weapons.add(self.weapon(self.rect.x, self.rect.y))
+            self.cool_down_counter = 0
 
 
 class BlueSpaceShip(SpaceShip):
-    pass
+    DEFAULT_COUNT = 10
+    COOLDOWN = 70
+
+    def __init__(self, pos_x, pos_y):
+        super(BlueSpaceShip, self).__init__(pos_x, pos_y)
+        self.speed = 5
+        self.set_images('blue_ship')
+        self.weapon = BlueSpaceShipBullet
 
 
 class RedSpaceShip(SpaceShip):
-    pass
+    DEFAULT_COUNT = 10
+    COOLDOWN = 60
+
+    def __init__(self, pos_x, pos_y):
+        super(RedSpaceShip, self).__init__(pos_x, pos_y)
+        self.speed = 7
+        self.set_images('red_ship')
+        self.weapon = RedSpaceShipBullet
 
 
 class GreenSpaceShip(SpaceShip):
-    pass
+    DEFAULT_COUNT = 15
+    COOLDOWN = 50
+
+    def __init__(self, pos_x, pos_y):
+        super(GreenSpaceShip, self).__init__(pos_x, pos_y)
+        self.speed = 8
+        self.set_images('green_ship')
+        self.weapon = GreenSpaceShipBullet
